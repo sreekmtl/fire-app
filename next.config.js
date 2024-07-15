4c80ff2774ef0d939f53180292a319a051d9b02f
sreekmtl <sreeharikollamattam@gmail.com>
2024-07-15 23:34:07 +0530

    all set

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
       return [
         {
           source: '/api/:path*',
-          destination: 'http://localhost:8080/:path*', // Change to your GeoServer URL
+          destination: 'http://localhost:8080/:path*', 
         },
       ];
     },
